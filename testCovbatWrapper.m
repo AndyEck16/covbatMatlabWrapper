7b889541bfbb516aee0e66592e5792df97dfbbf4
Andy <me@something.com>
2023-09-27 17:33:16 -0500

    Explicit variable for numROI of fc data

--- testCovbatWrapper.m
+++ testCovbatWrapper.m
@@ -1,12 +1,12 @@
 numSubj = 20;
+numROI = 40;
 
-testFcData = rand(40,40,numSubj);
+testFcData = rand(numROI,numROI,numSubj);
 testCovarTbl = table(rand(numSubj,1), round(rand(numSubj,1),0),rand(numSubj,1)*2, rand(numSubj,1)+2, ceil(rand(numSubj,1)*3), ...
     'VariableNames',{'numvar1','catvar2','numvar3','numvar4', 'batchVar'});
 testModelString = '~ numvar1 + catvar2*numvar3';
 colsInModelThatAreNumeric = {'numvar1','numvar3'};
 
-%testCovarTbl.numvar1(3) = NaN;
 
 newFc = runCovbatWrapper(...
                             testFcData, testCovarTbl, 'batchVar', ...
